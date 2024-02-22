795d0bc9ffac42e52cee0f2087baf9090f1cae59
John Kim A. Querobines <querobinesj@gmail.com>
2024-02-22 20:57:10 +0800

    copy and landing page update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,17 +16,17 @@ const config = {
         siteUrl: siteMetadata.siteUrl, // Used for sitemap generation
         manifestSettings: {
           favicon: './content/images/favicon.png', // Path is relative to the root
-          siteName: 'My Minimal Portfolio', // Used in manifest.json
-          shortName: 'Portfolio', // Used in manifest.json
+          siteName: siteMetadata.title, // Used in manifest.json
+          shortName: 'SFC', // Used in manifest.json
           startUrl: '/', // Used in manifest.json
           backgroundColor: '#FFFFFF', // Used in manifest.json
-          themeColor: '#000000', // Used in manifest.json
+          themeColor: '#405aac', // Used in manifest.json
           display: 'minimal-ui', // Used in manifest.json
         },
         contentDirectory: './content',
         blogSettings: {
-          path: '/blog', // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+          path: '/articles', // Defines the slug for the articles listing page
+          usePathPrefixForArticles: false, // Default true (i.e. path will be /articles/first-article)
         },
       },
     },
